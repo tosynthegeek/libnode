47e8f295a00d1afaac31940c4578743398e04887
tosynthegeek <oluwatosinojedapo@gmail.com>
2024-07-02 18:05:27 +0100

    Source node peers.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"libp2p/nodes"
 	"log"
 )
@@ -19,4 +20,14 @@ func main() {
 	}
 	nodes.NodeID(target)
 	nodes.NodeAddress(target)
+
+	nodes.ConnectNode(source, target)
+	fmt.Println("Nodes connected....")
+
+	peers, err:= nodes.SourceNodePeers(source)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("Source node peers: %d", peers)
 }
